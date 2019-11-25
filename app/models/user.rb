3aec552a6c9f742975dc39aefd78f805dd330963
kudojp <daiki.kudo@bebit.com>
2019-11-25 22:37:47 +0900

    Flash user edit, update index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: {case_sensitive: false}
   
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
   has_secure_password()
 
   # 永続的セッションで使用するユーザーをデータベースに記憶する
