7c29431b64527af91d9babbfbd471fc2b7c1a6d6
kudojp <daiki.kudo@bebit.com>
2019-12-01 01:07:31 +0900

    Update env-config of production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your.herokuapp.com'
+  host = 'nameless-beach-93953.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
